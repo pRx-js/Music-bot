7f64eed373d11a7a5593f96068e98f2e8d1115bb
Hamdullah ALTUN <50997909+pRx-js@users.noreply.github.com>
2020-05-07 18:50:07 +0300

    Update clear-queue.js

--- commands/clear-queue.js
+++ commands/clear-queue.js
@@ -2,17 +2,17 @@ const emotes = require ("../config/emojis.json");
 
 module.exports.run = async (client, message) => {
   
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`);
+    if(!message.member.voice.channel) return message.channel.send(`**Bir ses kanalında değilsiniz ${emotes.error}**`);
 
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`);
+    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**Oynatılan bir müzik yok ${emotes.error}**`);
 
     client.player.clearQueue(message.guild.id);
 
-    message.channel.send(`**Queue cleared ${emotes.success}**`);
+    message.channel.send(`**Kuyruk temizlendi ${emotes.success}**`);
 
 };
 
 module.exports.config = {
     name: "clear-queue",
-    aliases: []
+    aliases: ['kuyruktemizle','clearqueue','kuyrukboşalt','kuyruksil']
 };
